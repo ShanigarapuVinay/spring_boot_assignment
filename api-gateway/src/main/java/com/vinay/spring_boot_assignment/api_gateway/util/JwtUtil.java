034cf5f7823c43377c3a49e1ead0681442bb6a77
ShanigarapuVinay <vinay.shanigarapu@zemosolabs.com>
2024-09-19 16:33:18 +0530

    :bug: fix: remove hardcoded Jwt Secret in api-gateway

--- api-gateway/src/main/java/com/vinay/spring_boot_assignment/api_gateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/vinay/spring_boot_assignment/api_gateway/util/JwtUtil.java
@@ -10,7 +10,13 @@ import java.security.Key;
 
 @Component
 public class JwtUtil {
-    public static final String SECRET = "3ea841983b91224be6d03e20ccef470c28d127a045d538add683288e3baf2c649fd335c692430265988ae18b4d150933ffc8dfa15303c328ae322dbed88aa170a21f1c4f8aa9d24616a016a1a125970dba27e1091d0bf2749228727251f811ea108059b1774acb59308dbbb7fb90d0e742570ce33e846d1481a9ac15705a7270";
+
+    public static String SECRET;
+
+    @Value("${JWT_SECRET}")
+    public void setSecret(String secret) {
+        this.SECRET = secret;
+    }
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
