8d4158e4c13428249f205d07b0d5c950a954eeb3
ShanigarapuVinay <vinay.shanigarapu@zemosolabs.com>
2024-09-19 17:42:55 +0530

    :art: fix: change in api-gateway pom.xml and application.java

--- api-gateway/src/main/java/com/vinay/spring_boot_assignment/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/vinay/spring_boot_assignment/api_gateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.vinay.spring_boot_assignment.api_gateway;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
